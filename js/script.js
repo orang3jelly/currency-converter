be7413a58ea5416fd568dcfc43819cb153615a79
Mateusz Nowicki <m.nowicki941@gmail.com>
2024-02-24 22:20:31 +0100

    calculateResult function added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-console.log("🤑");
-
 let inputElement = document.querySelector(".js-inputPLN");
 let currencyElement = document.querySelector(".js-selectCurrency");
 let formElement = document.querySelector(".js-form");
 let resultElement = document.querySelector(".js-operation");
 
-let rateEUR = 4.3463;
-let rateUSD = 4.0495;
-let rateGBP = 5.0840;
-let rateCHF = 4.5849;
+const calculateResult = (input, currency) => {
+  let rateEUR = 4.3463;
+  let rateUSD = 4.0495;
+  let rateGBP = 5.084;
+  let rateCHF = 4.5849;
+
+  switch (currency) {
+    case "EUR":
+      return input / rateEUR;
+
+    case "USD":
+      return input / rateUSD;
+
+    case "GBP":
+      return input / rateGBP;
+
+    case "CHF":
+      return input / rateCHF;
+  }
+};
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -16,21 +30,6 @@ formElement.addEventListener("submit", (event) => {
   let input = +inputElement.value;
   let currency = currencyElement.value;
   let result;
-
-  switch (currency) {
-    case "EUR":
-      result = input / rateEUR;
-      break;
-    case "USD":
-      result = input / rateUSD;
-      break;
-    case "GBP":
-      result = input / rateGBP;
-      break;
-    case "CHF":
-      result = input / rateCHF;
-      break;
-  }
 
   resultElement.innerText = `${input} PLN = ${result.toFixed(2)} ${currency}`;
 });
